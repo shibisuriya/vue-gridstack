c74b57a6fea2e45d2943b5b06d07566b31aa0d9f
Shibi Suriya <shibisuriya.work@gmail.com>
2022-06-20 15:31:17 +0530

    Nested grid prototype

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,18 @@
 	<div>
 		<gridstack-layout>
 			<gridstack-item v-for="(item, index) in layout" :key="index" :item="item">
-				<h1>{{index}}</h1>
+				<gridstack-layout v-if="item.hasOwnProperty('section')">
+					<gridstack-item v-for="(item2, index2) in item.section" :item="item" :key="index2">
+						<ComponentA v-if="item2.component == 'a'" />
+						<ComponentB v-if="item2.component == 'b'" />
+						<ComponentC v-if="item2.component == 'c'" />
+					</gridstack-item>
+				</gridstack-layout>
+				<template v-else>
+					<ComponentA v-if="item.component == 'a'" />
+					<ComponentB v-if="item.component == 'b'" />
+					<ComponentC v-if="item.component == 'c'" />
+				</template>
 			</gridstack-item>
 		</gridstack-layout>
 	</div>
@@ -12,27 +23,75 @@
 import GridstackLayout from "./components/GridstackLayout.vue";
 import GridstackItem from "./components/GridstackItem.vue";
 import GridStack from '/node_modules/gridstack/dist/gridstack-h5.js'
+
+
+import ComponentA from './widgets/ComponentA.vue';
+import ComponentB from './widgets/ComponentB.vue'
+import ComponentC from './widgets/ComponentC.vue';
+
 export default {
 	components: {
 		GridstackLayout,
 		GridstackItem,
+
+		ComponentA,
+		ComponentB,
+		ComponentC
 	},
 	mounted() {
 		const self = this;
-		self.grid = GridStack.init({ float: true, cellHeight: '70px', minRow: 3, margin: 10, column: 11});
+		self.grid = GridStack.initAll({ float: true, cellHeight: '70px', minRow: 3, margin: 10, column: 11, acceptWidgets: true });
 	},
 	data() {
 		return {
 			layout: [
 				{
-					id: 1,
 					x: 0,
 					y: 0,
 					w: 2,
 					h: 2,
+					component: 'a'
 				},
 				{
-					id: 2,
+					x: 1,
+					y: 2,
+					w: 2,
+					h: 2,
+					component: 'b'
+				},
+				{
+					section: [{
+						x: 1,
+						y: 2,
+						w: 2,
+						h: 2,
+						component: 'c'
+					}, {
+						x: 0,
+						y: 0,
+						w: 2,
+						h: 2,
+						component: 'a'
+					}],
+					x: 1,
+					y: 2,
+					w: 2,
+					h: 2,
+				},
+				{
+					section: [{
+						x: 1,
+						y: 2,
+						w: 2,
+						h: 2,
+						component: 'c'
+					}, {
+						x: 0,
+						y: 0,
+						w: 2,
+						h: 2,
+						component: 'a'
+					}],
 					x: 1,
 					y: 2,
 					w: 2,
---
+++ src/widgets/ComponentC.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>
+            Component C
+        </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
