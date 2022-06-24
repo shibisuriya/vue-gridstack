9b364c9961d66339fa9a213368a7c74bdb829811
Shibi Suriya <shibisuriya.work@gmail.com>
2022-06-24 20:14:53 +0530

    ADDED sourcemap

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,13 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   configureWebpack: (config) => {
     config.devtool = "eval-source-map";
+
+    config.output.devtoolModuleFilenameTemplate = (info) =>
+      info.resourcePath.match(/^\.\/\S*?\.vue$/)
+        ? `webpack-generated:///${info.resourcePath}?${info.hash}`
+        : `webpack-yourCode:///${info.resourcePath}`;
+
+    config.output.devtoolFallbackModuleFilenameTemplate =
+      "webpack:///[resource-path]?[hash]";
   },
 });
