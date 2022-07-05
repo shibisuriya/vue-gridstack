eb0dca972df94b2a2dec7a755d2019c455b9bd11
Shibi Suriya <shibisuriya.work@gmail.com>
2022-07-05 15:01:26 +0530

    Updated dummy layout

--- src/App.vue
+++ src/App.vue
@@ -19,17 +19,17 @@
     <!-- Modifying the props from within a child component is a bad practice, if you want to get the current state of the grids,
 		use @save event. The props passed into the gridstack-layout won't sync automatically (This method is followed in vue-grid-stack). -->
     <gridstack-layout @save="saveGrid" ref="gridstackLayout" :layout="layout" :static="false">
-      <gridstack-item v-for="(section, index) in layout" :key="index" :item="section" @removeWidget="removeWidget">
-        <gridstack-section v-if="section.hasOwnProperty('section')" @shrink="shrink">
+      <gridstack-item v-for="(section, index) in layout" :key="index" :item="section">
+        <gridstack-section v-if="section.hasOwnProperty('section')">
           <gridstack-item v-for="(child, childIndex) in section.section" :item="child" :key="childIndex">
             <!-- Event is not emitted from componentA here...  -->
-            <ComponentA v-if="child.component == 'a'" @testEvent="aEvent" />
+            <ComponentA v-if="child.component == 'a'" />
             <ComponentB v-if="child.component == 'b'" />
             <ComponentC v-if="child.component == 'c'" />
           </gridstack-item>
         </gridstack-section>
         <template v-else>
-          <ComponentA v-if="section.component == 'a'" @testEvent="aEvent" />
+          <ComponentA v-if="section.component == 'a'" />
           <ComponentB v-if="section.component == 'b'" />
           <ComponentC v-if="section.component == 'c'" />
         </template>
@@ -60,26 +60,10 @@ export default {
   },
   methods: {
     addWidget() {
-      this.layout.push({ x: 0, y: 0, w: 34, h: 14, component: "a" });
-    },
-    aEvent() {
-      console.log("hello");
+      this.layout.push({ "x": 0, "y": 0, "w": 37, "h": 16, "component": "a" });
     },
     addSection() {
-      this.layout.push({
-        x: 0,
-        y: 14,
-        w: 96,
-        h: 5,
-        maxW: 96,
-        minW: 96,
-        noResize: true,
-        collapsed: false,
-        section: [{ x: 0, y: 0, w: 51, h: 16, component: "a" }],
-      });
-    },
-    shrink(el) {
-      this.$refs["gridstackLayout"].shrink(el);
+      this.layout.push({ "x": 0, "y": 0, "w": 96, "h": 23, "maxW": 96, "minW": 96, "noResize": true, "collapsed": false, "section": [{ "x": 0, "y": 0, "w": 49, "h": 17, "component": "c" }] });
     },
     saveGrid(gridData) {
       this.gridData = serialize(gridData);
@@ -97,15 +81,15 @@ export default {
         x: 0,
         y: 16,
         w: 96,
-        h: 22,
+        h: 5,
         maxW: 96,
         minW: 96,
         noResize: true,
-        collapsed: false,
+        collapsed: true,
         section: [{ x: 0, y: 0, w: 48, h: 21, component: "c" }],
       },
     ],
-    console.log(this.layout);
+      console.log(this.layout);
   },
   data() {
     return {
